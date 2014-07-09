113fa9edbbbfdb5f1193a55bc4ef58b42f0a8808
mishudark <mishu@moon>
2014-07-08 23:17:21 -0500

    add(go files): baseController and auth

---
+++ authbackend.go
@@ -0,0 +1,75 @@
+package base
+
+import (
+  "code.google.com/p/go.crypto/scrypt"
+  "encoding/json"
+  "github.com/astaxie/beego/session"
+  "net/http"
+  "regexp"
+  "time"
+)
+
+var localSession *session.Manager
+
+type User struct {
+  Username string
+  pass string
+  Is_anonymous bool
+  Is_autenthicated bool
+  Last_login string
+  Perms []string
+}
+
+func (self *User) Anonymous() {
+  self.Username = "Anonymous"
+  self.pass = ""
+  self.Is_anonymous = true
+  self.Is_autenthicated = false
+  self.Last_login = ""
+}
+
+func encryptpass(pass string) string {
+  salt := "$"
+  key, _ := scrypt.Key([]byte(pass), []byte(salt), 16384, 8, 1, 32)
+
+  return string(key[:])
+}
+
+func (self *User) check(username, pass string) bool {
+  //TODO: query para obtener el usuario 
+  /*
+  if self.pass == encryptpass(pass) {
+    return true
+  }
+  */
+  return false
+}
+
+func (self *User) checkUsername(username string) bool {
+  if ok, _ := regexp.MatchString("^[a-zA-Z0-9]{1,32}$", username); !ok {
+    return false
+  }
+  return true
+}
+
+func (self *User) authenticate(username, pass string) bool {
+  if !self.check(username, pass) {
+    return false
+  }
+  return true
+}
+
+func (self *User) login(w http.ResponseWriter, r *http.Request) bool {
+  now := time.Now().Format("2006-01-02 15:04:05")
+
+  self.Last_login = now
+  self.Is_autenthicated = true
+  self.Is_anonymous = false
+
+  //sess := globalSessions.SessionStart(userCtrl.Ctx.ResponseWriter, userCtrl.Ctx.Request)
+  sess := localSession.SessionStart(w, r)
+  encoded, _ := json.Marshal(self)
+  sess.Set("user", encoded)
+
+  return true
+}
---
+++ base.go
@@ -0,0 +1,43 @@
+package base
+
+import (
+  //"encoding/json"
+  //"fmt"
+	"github.com/astaxie/beego"
+  "github.com/astaxie/beego/session"
+  "time"
+)
+
+const (
+  cookie = "gosessionid"
+)
+
+var globalSessions *session.Manager
+
+type BaseController struct {
+	beego.Controller
+  sess session.SessionStore
+}
+
+func (self *BaseController) Session() {
+  globalSessions, _ = session.NewManager("memory", `{"cookieName":"gosessionid", "enableSetCookie,omitempty": true, "gclifetime":3600, "maxLifetime": 3600, "secure": false, "sessionIDHashFunc": "sha1", "sessionIDHashKey": "", "cookieLifeTime": 3600, "providerConfig": ""}`)
+  go globalSessions.GC()
+
+  if self.Ctx.GetCookie(cookie) == "" {
+    return
+  }
+
+  w := self.Ctx.ResponseWriter
+  r := self.Ctx.Request
+  sess := globalSessions.SessionStart(w, r)
+
+  createtime := sess.Get("createtime")
+  if createtime == nil {
+    sess.Set("createtime", time.Now().Unix())
+  } else if (createtime.(int64) + 60) < (time.Now().Unix()) {
+    globalSessions.SessionDestroy(w, r)
+    sess = globalSessions.SessionStart(w, r)
+  }
+
+  self.sess = sess
+}
